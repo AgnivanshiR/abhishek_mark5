2d50961142608304c9fd6c8d087721b14d46d36f
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-21 22:56:57 +0530

    initialcommit-clickevent-consol

---
+++ app.js
@@ -0,0 +1,9 @@
+var txtInput = document.querySelector(".txt-input");
+var btnTranslate = document.querySelector(".btn-translate");
+var output = document.querySelector('.output');
+
+function clickHandler(){
+    console.log("clicked!")
+}
+
+btnTranslate.addEventListener("click", clickHandler)
