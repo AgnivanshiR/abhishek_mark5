37420c1487d8ba806c2d3a3f0a4023727a996024
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-21 23:03:30 +0530

    text-input-consol

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var txtInput = document.querySelector(".txt-input");
 var btnTranslate = document.querySelector(".btn-translate");
 var output = document.querySelector('.output');
 
-function clickHandler(){
-    console.log("clicked!")
+// function clickHandler(){
+//     console.log("clicked!")
+// }
+function clickHandler () {
+    console.log(txtInput.value)
 }
 
 btnTranslate.addEventListener("click", clickHandler)
